da14a36d9f639ad6a53dc3d14b63df509e183a70
Danel <danieldob200@gmail.com>
2022-07-29 17:13:17 -0800

    hashrouter

--- src/App.js
+++ src/App.js
@@ -2,25 +2,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   BrowserRouter,
   Routes,
-  Route,
+  Route
 } from "react-router-dom";
 
 import Main from './pages/main';
 import About from './pages/about';
 import Services from './pages/services';
 import Contact from './pages/contact';
-
+import {HashRouter as Router} from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}> 
+    <Router basename={process.env.PUBLIC_URL}> 
       <Routes>
         <Route path="/" element={<Main />} />        
         <Route path="/services" element={<Services/>} />        
         <Route path="/about" element={<About />} />        
         <Route path="/contact" element={<Contact />} />                        
       </Routes>    
-    </BrowserRouter>        
+    </Router>        
   );
 }
 
